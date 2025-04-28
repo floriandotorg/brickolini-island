79009a7183818a96322b9910fabe95c2c27cdf00
Florian Kaiser <mail@floriankaiser.org>
2025-04-28 16:35:43 +0200

    Load all si files

--- src/lib/assets.ts
+++ src/lib/assets.ts
@@ -4,14 +4,62 @@ import { BinaryWriter } from './binary-writer'
 import { Smk } from './smk'
 import { BinaryReader } from './binary-reader'
 import { WDB } from './wdb'
-let si: SI | null = null
+import { setLoading } from './store'
+
+const siFiles: Map<string, SI> = new Map()
 let wdb: WDB | null = null
 
 export const initAssets = async (file: File) => {
-  const iso = new ISO9660(await file.arrayBuffer(), ISOVariant.Joliet)
-  si = new SI(iso.open('Lego/Scripts/INTRO.SI'))
+  const updateLoading = async (progress: number, message: string) => {
+    setLoading({ progress, message })
+    // hack to update the UI, todo move loading to web worker
+    await new Promise(resolve => setTimeout(resolve))
+  }
 
-  wdb = new WDB(iso.open("DATA/disk/LEGO/data/WORLD.WDB"))
+  await updateLoading(0, 'Loading ISO...')
+
+  const iso = new ISO9660(await file.arrayBuffer(), ISOVariant.Joliet)
+
+  const filenames = [
+    'Lego/Scripts/INTRO.SI',
+    'Lego/Scripts/Build/COPTER.SI',
+    'Lego/Scripts/Build/DUNECAR.SI',
+    'Lego/Scripts/Build/JETSKI.SI',
+    'Lego/Scripts/Build/RACECAR.SI',
+    'Lego/Scripts/Race/CARRACE.SI',
+    'Lego/Scripts/Race/CARRACER.SI',
+    'Lego/Scripts/Race/JETRACE.SI',
+    'Lego/Scripts/Race/JETRACER.SI',
+    'Lego/Scripts/Isle/ISLE.SI',
+    'Lego/Scripts/Infocntr/ELEVBOTT.SI',
+    'Lego/Scripts/Infocntr/INFODOOR.SI',
+    'Lego/Scripts/Infocntr/INFOMAIN.SI',
+    'Lego/Scripts/Infocntr/INFOSCOR.SI',
+    'Lego/Scripts/Infocntr/REGBOOK.SI',
+    'Lego/Scripts/Infocntr/HISTBOOK.SI',
+    'Lego/Scripts/Hospital/HOSPITAL.SI',
+    'Lego/Scripts/Police/POLICE.SI',
+    'Lego/Scripts/Garage/GARAGE.SI',
+    'Lego/Scripts/Act2/ACT2MAIN.SI',
+    'Lego/Scripts/Act3/ACT3.SI',
+    'Lego/Scripts/Isle/JUKEBOX.SI',
+    'Lego/Scripts/Isle/JUKEBOXW.SI',
+    'Lego/Scripts/SNDANIM.SI',
+    'Lego/Scripts/CREDITS.SI',
+  ]
+  for (let n = 0; n < filenames.length; n++) {
+    const filename = filenames[n].split('/').pop()
+    if (filename == null) {
+      throw new Error('Filename not found')
+    }
+    await updateLoading(5 + (n / filenames.length) * 90, `Loading ${filename}...`)
+    siFiles.set(filename, new SI(iso.open(filenames[n])))
+  }
+
+  await updateLoading(95, 'Loading WDB...')
+  wdb = new WDB(iso.open('DATA/disk/LEGO/data/WORLD.WDB'))
+
+  setLoading(null)
 }
 
 const createWAV = (obj: SIObject): ArrayBuffer => {
@@ -39,6 +87,7 @@ const createWAV = (obj: SIObject): ArrayBuffer => {
 }
 
 export const getMovie = (name: string): { audio: ArrayBuffer; video: Smk } => {
+  const si = siFiles.get('INTRO.SI')
   if (si == null) {
     throw new Error('Assets not initialized')
   }
--- src/lib/store.ts
+++ src/lib/store.ts
@@ -1,17 +1,22 @@
 import Alpine from 'alpinejs'
 
 export type Mode = 'welcome' | 'loading' | 'in-game' | 'cutscene'
+export type Loading = {
+  progress: number
+  message: string
+} | null
 
 export const initStores = () => {
   Alpine.store('state', {
     mode: 'welcome' as Mode,
-
-    setMode(mode: Mode) {
-      ;(this as { mode: Mode }).mode = mode
-    },
+    loading: null,
   })
 }
 
 export const setMode = (mode: Mode) => {
   ;(Alpine.store('state') as { mode: Mode }).mode = mode
+}
+
+export const setLoading = (loading: Loading) => {
+  ;(Alpine.store('state') as { loading: Loading }).loading = loading
 }
