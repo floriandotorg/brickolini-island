6a3cc4e81061f508f3ff1dc0dbf80d0d089e5987
Florian Kaiser <mail@floriankaiser.org>
2025-04-28 13:54:27 +0200

    Add caching for iso

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,18 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-	base: '',
+  base: '',
+  plugins: [
+    {
+      name: 'cache-iso',
+      configureServer: server => {
+        server.middlewares.use((req, res, next) => {
+          if (req.originalUrl.toLowerCase().includes('.iso')) {
+            res.setHeader('Cache-Control', 'public, max-age=31536000, immutable')
+          }
+          next()
+        })
+      },
+    },
+  ],
 })
