d23ec87e725ae4e08889121a5ef24ac902b3ec1e
Florian Kaiser <mail@floriankaiser.org>
2025-07-06 12:37:32 +0200

    Add hospital lights

--- src/worlds/hospital.ts
+++ src/worlds/hospital.ts
@@ -1,3 +1,4 @@
+import * as THREE from 'three'
 import { Background_Bitmap, hho006cl_RunAnim } from '../actions/hospital'
 import { playAnimation } from '../lib/animation'
 import { Building } from '../lib/world/building'
@@ -7,6 +8,14 @@ export class Hospital extends Building {
     await super.init()
 
     await this.loadWorld('HOSP', Background_Bitmap)
+
+    const rightPointLight = new THREE.PointLight(0xfefefe, 20)
+    rightPointLight.position.set(-6.8, 1, 1)
+    this._scene.add(rightPointLight)
+
+    const leftPointLight = new THREE.PointLight(0xfefefe, 20)
+    leftPointLight.position.set(-0.25, 1, 1)
+    this._scene.add(leftPointLight)
 
     await playAnimation(this, hho006cl_RunAnim)
   }
