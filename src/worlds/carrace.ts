efccb989d130a06eb115a5306a84efb107842140
Fabian Neundorf <CommodoreFabianus@gmx.de>
2026-01-02 21:19:23 +0100

    Add map of race with locators

--- src/worlds/carrace.ts
+++ src/worlds/carrace.ts
@@ -1,12 +1,90 @@
-import type * as THREE from 'three'
-import { irtx08ra_PlayWav, srt001rh_RunAnim, srt001sl_RunAnim, srt002rh_RunAnim, srt002sl_RunAnim, srt003rh_RunAnim, srt003sl_RunAnim, srt004sl_RunAnim, srt005sl_RunAnim } from '../actions/carrace'
+import * as THREE from 'three'
+import { irtx08ra_PlayWav, Map_Down_Bitmap, Rhoda_Locator, Studs_Locator, srt001rh_RunAnim, srt001sl_RunAnim, srt002rh_RunAnim, srt002sl_RunAnim, srt003rh_RunAnim, srt003sl_RunAnim, srt004sl_RunAnim, srt005sl_RunAnim, User_Locator, UserCar_Actor } from '../actions/carrace'
 import { RaceTrackRoad_Music } from '../actions/jukebox'
-import type { Composer } from '../lib/effect/composer'
-import { engine, type NormalizedMouseEvent } from '../lib/engine'
+import { getExtraValue, type ImageAction, splitExtraValue } from '../lib/action-types'
+import { createImageSprite } from '../lib/assets/canvas-sprite'
+import { type Composer, Render2D } from '../lib/effect/composer'
+import { engine, type NormalizedMouseEvent, type NormalizedRect, normalizeRect } from '../lib/engine'
 import { PlayerMovement } from '../lib/world/player-movement'
 import { IsleBase } from './isle-base'
 
 const introAnimations = [srt001sl_RunAnim, srt002sl_RunAnim, srt003sl_RunAnim, srt004sl_RunAnim, srt005sl_RunAnim, srt001rh_RunAnim, srt002rh_RunAnim, srt003rh_RunAnim]
+
+class RaceMap {
+  private readonly _group: THREE.Group
+  private readonly _locatorMap: Map<string, ImageAction>
+  private readonly _locators: MapLocator[] = []
+
+  public constructor(locatorMap: Map<string, ImageAction>) {
+    this._locatorMap = locatorMap
+    this._group = new THREE.Group()
+    this._group.visible = false
+  }
+
+  public addLocator(action: { extra: string }, object: THREE.Object3D) {
+    const locator = new MapLocator(action, this._locatorMap, object)
+    this._group.add(locator.sprite)
+    this._locators.push(locator)
+  }
+
+  public update() {
+    for (const locator of this._locators) {
+      locator.update()
+    }
+  }
+
+  public get group(): THREE.Group {
+    return this._group
+  }
+}
+
+class MapLocator {
+  private readonly _worldXOffset: number
+  private readonly _worldXLength: number
+  private readonly _worldZOffset: number
+  private readonly _worldZLength: number
+  private readonly _mapRect: NormalizedRect
+  private readonly _sprite: THREE.Sprite
+  private readonly _object: THREE.Object3D
+
+  public constructor(action: { extra: string }, locatorMap: Map<string, ImageAction>, object: THREE.Object3D) {
+    const locatorName = getExtraValue(action, 'Map_Locator')
+    if (locatorName == null) {
+      throw new Error('No map locator defined')
+    }
+    const locatorImage = locatorMap.get(locatorName)
+    if (locatorImage == null) {
+      throw new Error('No map locator defined')
+    }
+    const geometry = getExtraValue(action, 'Map_Geometry')
+    if (geometry == null) {
+      throw new Error('No map geometry defined')
+    }
+    const [worldXOffset, worldXLength, worldZOffset, worldZLength, mapWidth, mapHeight, mapXOffset, mapYOffset] = splitExtraValue(geometry).map(s => parseInt(s, 10))
+    this._worldXOffset = worldXOffset
+    this._worldXLength = worldXLength
+    this._worldZOffset = worldZOffset
+    this._worldZLength = worldZLength
+    this._mapRect = normalizeRect(mapXOffset, mapYOffset, mapWidth, mapHeight)
+
+    this._sprite = createImageSprite(locatorImage, -0.25)
+    this._object = object
+  }
+
+  public get sprite(): THREE.Sprite {
+    return this._sprite
+  }
+
+  public update(): void {
+    const worldPosition = this._object.getWorldPosition(new THREE.Vector3())
+    const x = (-worldPosition.x - this._worldXOffset) / this._worldXLength
+    const z = -(worldPosition.z - this._worldZOffset) / this._worldZLength
+    if (x >= 0 && x <= 1 && z >= 0 && z <= 1) {
+      this._sprite.position.x = this._mapRect.normalizedX + x * this._mapRect.normalizedWidth
+      this._sprite.position.y = this._mapRect.normalizedY - z * this._mapRect.normalizedHeight
+    }
+  }
+}
 
 export class CarRace extends IsleBase {
   private _playerMovement = new PlayerMovement(
@@ -15,6 +93,8 @@ export class CarRace extends IsleBase {
     () => this._boundaryManager.walls,
     () => this._isleMesh,
   )
+  private readonly _controlsRender = new Render2D()
+  private _raceMap: RaceMap | null = null
 
   constructor() {
     super('carrace', 'RACC')
@@ -29,6 +109,19 @@ export class CarRace extends IsleBase {
     void this.playAnimation(introAnimations[Math.floor(Math.random() * introAnimations.length)]).then(() => {
       void engine.playAudio(irtx08ra_PlayWav, 'speech')
     })
+
+    const locatorImages = new Map<string, ImageAction>([
+      [Rhoda_Locator.name, Rhoda_Locator],
+      [Studs_Locator.name, Studs_Locator],
+      [User_Locator.name, User_Locator],
+    ])
+    this._raceMap = new RaceMap(locatorImages)
+    this._controlsRender.scene.add(this._raceMap.group)
+    this._controlsRender.scene.add(createImageSprite(Map_Down_Bitmap, -0.5))
+    composer.add(this._controlsRender)
+
+    const actor = UserCar_Actor
+    this._raceMap.addLocator(actor, this.camera)
   }
 
   protected override get debugPositionDirection(): { position: THREE.Vector3; direction: THREE.Vector3; slewMode: boolean } | null {
@@ -62,5 +155,7 @@ export class CarRace extends IsleBase {
 
     const { normalizedSpeed } = this._playerMovement.update(delta, 'racecar')
     this._dashboard.update(normalizedSpeed)
+
+    this._raceMap?.update()
   }
 }
