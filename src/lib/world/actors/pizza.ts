d393a0ac72937bc567d139cf72ee1d8c7bb54e24
Florian Kaiser <mail@floriankaiser.org>
2026-03-11 12:05:51 +0100

    Small refactor

--- src/lib/world/actors/pizza.ts
+++ src/lib/world/actors/pizza.ts
@@ -3,11 +3,7 @@ import { Pizzeria } from './pizzeria'
 
 export class Pizza extends Actor {
   public override async onClick(): Promise<boolean> {
-    const pizzeria = this._isle.getRoi('pizza').actor
-    if (!(pizzeria instanceof Pizzeria)) {
-      throw new Error('Pizzeria is not a Pizzeria')
-    }
-    pizzeria.onPizzaClicked()
+    this._isle.getActor('pizza', Pizzeria).onPizzaClicked()
     return true
   }
 }
